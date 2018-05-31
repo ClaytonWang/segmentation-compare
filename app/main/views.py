43f2c0fc197845f595b4d5adae83c993f4832aa9
clay.wang <lovejunlic@qq.com>
2018-05-31 17:50:08 +0800

    update

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from flask import render_template, request, redirect, url_for, make_response, se
 from . import main
 from .forms import NameForm
 from .. import db
-from ..models import Survey,SurveyScore
+from ..models import Survey, SurveyScore
 from .common import allowed_file, get_file_extension, wam, segment, get_filename, read_file, write_excel_segmentation, write_excel, save_file
 import json
 import os
@@ -23,7 +23,8 @@ UPLOAD_MARKLOGIC_FILEPATH = 'uploads/marklogics'
 @main.route('/')
 def index():
     array = []
-    datasets = Survey.query.filter_by(type_code=0).order_by(Survey.id.asc()).all()
+    datasets = Survey.query.filter_by(
+        type_code=0).order_by(Survey.id.asc()).all()
     for item in datasets:
         setattr(item, 'survey', json.loads(item.json_content))
         array.append(item)
@@ -33,7 +34,8 @@ def index():
 @main.route('/surveylist/<string:parent_id>', methods=['GET'])
 def surveylist(parent_id):
     array = []
-    surveys = Survey.query.filter_by(type_code=1,parent_pkey=parent_id).order_by(Survey.id.asc()).all()
+    surveys = Survey.query.filter_by(
+        type_code=1, parent_pkey=parent_id).order_by(Survey.id.asc()).all()
     for item in surveys:
         setattr(item, 'survey', json.loads(item.json_content))
         array.append(item)
@@ -44,13 +46,29 @@ def surveylist(parent_id):
 @main.route('/survey/<int:svy_id>', methods=['GET'])
 def survey(svy_id):
     survey = Survey.query.get(svy_id)
-    dataset = Survey.query.filter_by(type_code=0,pkey=survey.parent_pkey).first()
-    svy_score = SurveyScore.query.filter_by(survey_id=svy_id).order_by(SurveyScore.id.desc()).first()
+    dataset = Survey.query.filter_by(
+        type_code=0, pkey=survey.parent_pkey).first()
+    svy_score = SurveyScore.query.filter_by(
+        survey_id=svy_id).order_by(SurveyScore.id.desc()).first()
 
     info = json.loads(dataset.json_content)
     svy_content = json.loads(survey.json_content)
-    
-    return render_template('survey.html', survey=svy_content,info=info['info'], svy_score = svy_score, svy_id=svy_id,parent_pkey=survey.parent_pkey)
+
+    return render_template('survey.html', survey=svy_content, info=info['info'], svy_score=svy_score, svy_id=svy_id, parent_pkey=survey.parent_pkey)
+
+
+@main.route('/surveyids/<string:parent_id>', methods=['GET'])
+def surveyids(parent_id):
+    array = []
+    try:
+        surveys = Survey.query.filter_by(
+            type_code=1, parent_pkey=parent_id).order_by(Survey.id.asc()).all()
+        for item in surveys:
+            array.append(item.id)
+        data = {'result': 'Success', 'ids': array}
+    except Exception, e:
+        data = {'result': 'Error', 'message': e.message}
+    return json.dumps(data)
 
 
 @main.route('/survey/completed/<int:svy_id>', methods=['POST'])
@@ -58,19 +76,23 @@ def survey_completed(svy_id):
     try:
         score = request.form.get('score')
         comments = request.form.get('comments')
-        svy_score = SurveyScore.query.filter_by(survey_id=svy_id).order_by(SurveyScore.id.desc()).first()
+        status = request.form.get('status')
+        svy_score = SurveyScore.query.filter_by(
+            survey_id=svy_id).order_by(SurveyScore.id.desc()).first()
         if svy_score == None:
-            svy_score = SurveyScore(survey_id = svy_id, score=score, status=0, comments=comments, lastupdate_date = datetime.utcnow())
+            svy_score = SurveyScore(survey_id=svy_id, score=score, status=status,
+                                    comments=comments, lastupdate_date=datetime.utcnow())
             db.session.add(svy_score)
         else:
             svy_score.score = score
             svy_score.comments = comments
+            svy_score.status = status
             svy_score.lastupdate_date = datetime.utcnow()
         db.session.commit()
 
         data = {'result': 'Success'}
     except Exception, e:
-        data = {'result': 'Error','message':e.message}
+        data = {'result': 'Error', 'message': e.message}
     return json.dumps(data)
 
 
@@ -79,7 +101,7 @@ def survey_completed_succ():
     return render_template('survey_completed_succ.html')
 
 
-@main.route('/upload_json', methods=['GET','POST'])
+@main.route('/upload_json', methods=['GET', 'POST'])
 def upload_json():
     if request.method == 'GET':
         return render_template('upload_json.html')
@@ -87,18 +109,20 @@ def upload_json():
         try:
             pkey = str(uuid.uuid4())
             json_text = request.form.get('json_text')
-            dataset_svy = Survey(pkey = pkey,parent_pkey='0',json_content = json_text,type_code = 0,lastupdate_date=datetime.utcnow())
+            dataset_svy = Survey(pkey=pkey, parent_pkey='0', json_content=json_text,
+                                 type_code=0, lastupdate_date=datetime.utcnow())
             db.session.add(dataset_svy)
             db.session.commit()
-            
+
             dataset_content = json.loads(json_text)
             group = dataset_content['group']
-            for item in group :
-                query_svy = Survey(pkey = str(uuid.uuid4()),parent_pkey = pkey,json_content = json.dumps(item),type_code = 1,lastupdate_date = datetime.utcnow())
+            for item in group:
+                query_svy = Survey(pkey=str(uuid.uuid4()), parent_pkey=pkey, json_content=json.dumps(
+                    item), type_code=1, lastupdate_date=datetime.utcnow())
                 db.session.add(query_svy)
                 db.session.commit()
 
             data = {'result': 'Success'}
         except Exception, e:
-            data = {'result': 'Error','message':e.message}
+            data = {'result': 'Error', 'message': e.message}
         return json.dumps(data)
--- app/static/survey.js
+++ app/static/survey.js
@@ -12,7 +12,7 @@ $(function () {
     });
 
 
-    $('#submit_svy').on('click', function () {
+    $('.submit_svy').on('click', function () {
         var commtens = '';
         $('textarea').each(function (i, o) {
             commtens += encodeURIComponent($(o).val()) + '|||'
@@ -29,11 +29,14 @@ $(function () {
             }
         }
 
+        var status = arrScore.length == $('textarea').length ? '1' : '0';
         var data = {
             score: arrScore.join('|||'),
-            comments: commtens
+            comments: commtens,
+            status: status
         };
         var id = $('#svy_id').val();
+        var nextsvyid = $(this).attr('nextsvyid');
         $.ajax({
             type: "POST",
             url: "/survey/completed/" + id,
@@ -41,9 +44,12 @@ $(function () {
             dataType: "json",
             success: function (data) {
                 if (data['result'] === 'Success') {
-                    alert("Thank you! Your survey has been submit successful!");
-                    //document.location = '/survey/survey_completed_succ';
-                    document.location.reload(true);
+                    if (!nextsvyid) {
+                        alert("Thank you! Your survey has been submit successful!");
+                        document.location.reload(true);
+                    } else {
+                        document.location = '/survey/' + nextsvyid;
+                    }
                 } else {
                     alert("Error!");
                 }
@@ -123,4 +129,69 @@ $(function () {
     };
 
     init();
+
+    var setNextPrevPage = function (svy_ids) {
+        var curid = $('#svy_id').val();
+        var previd = '';
+        var nextid = '';
+        if (svy_ids.length) {
+            var index = svy_ids.findIndex(function (v, i) {
+                return (v == curid);
+            });
+
+            if (index === svy_ids.length - 1) {
+                //last
+                nextid = '';
+                $('.pager .next').addClass('disabled');
+
+                previd = svy_ids[index - 1];
+                if (previd) {
+                    $('.pager .previous a').attr('href', '/survey/' + previd);
+                } else {
+                    $('.pager .previous').addClass('disabled');
+                }
+            } else if (index === 0) {
+                //first
+                previd = '';
+                $('.pager .previous').addClass('disabled');
+
+                nextid = svy_ids[index + 1];
+                if (nextid) {
+                    $('.pager .next a').attr('href', '/survey/' + nextid);
+                } else {
+                    $('.pager .next').addClass('disabled');
+                }
+            } else {
+                nextid = svy_ids[index + 1];
+                previd = svy_ids[index - 1];
+                $('.pager .previous a').attr('href', '/survey/' + previd);
+                $('.pager .next a').attr('href', '/survey/' + nextid);
+            }
+
+            if (nextid) {
+                $('.btn-primary.submit_svy').attr('nextsvyid', nextid);
+            } else {
+                $('.btn-primary.submit_svy').attr('disabled', true);
+            }
+        }
+    };
+
+    var getSurveyIds = function () {
+        $.ajax({
+            type: "GET",
+            url: "/surveyids/" + $('#parent_pkey').val(),
+            dataType: "json",
+            success: function (data) {
+                if (data['result'] === 'Success') {
+                    setNextPrevPage(data['ids']);
+                } else {
+                    alert("Error!");
+                    console.log(data);
+                }
+            }
+        });
+    };
+    getSurveyIds();
+
+
 });
